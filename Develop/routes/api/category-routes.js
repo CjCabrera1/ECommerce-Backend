a511afbe3e1652d5f3f638abced151c610e0a8e7
cj cabrera <cabrera012500@gmail.com>
2023-10-27 13:21:34 -0500

    fixed categories to display the categories

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -20,10 +20,18 @@ router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
-    const newCategory = await Category.create(req.body);
-    res.status(201).json(newCategory);
+    const category = await Category.findByPk(req.params.id, {
+      include: Product, // Include associated Products
+    });
+
+    if (!category) {
+      res.status(404).json({ message: 'Category not found' });
+      return;
+    }
+
+    res.status(200).json(category);
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
 });
 
